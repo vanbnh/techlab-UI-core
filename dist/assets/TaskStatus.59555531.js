5cba0d191e046b3cc836afcc408755e1978af261
vanbnh <bui_ngoc_huy_van@ca-adv.co.jp>
2023-01-17 16:41:49 +0700

    change be ip

--- dist/assets/TaskStatus.59555531.js
+++
@@ -1,1 +0,0 @@
-import{j as t,B as a}from"./index.cbdf8a16.js";import{D as o}from"./index.739fac03.js";const r={success:"success",error:"danger",failed:"secondary",done:"info"},e=({row:s})=>t(a,{color:`light-${r[s.task_status]}`,children:s.task_status}),d=s=>t(o,{for:["task_status"],formatterComponent:e,...s});export{d as T};
