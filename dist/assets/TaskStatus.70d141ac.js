460bb0d7df5548a71037b1d8a5a8211e56aa1093
vanbnh <bui_ngoc_huy_van@ca-adv.co.jp>
2023-03-06 12:55:10 +0700

    add build staging

--- dist/assets/TaskStatus.70d141ac.js
+++
@@ -1,1 +0,0 @@
-import{j as t,au as a}from"./index.09cac9f7.js";import{D as o}from"./react-paginate.4764f00c.js";const r={success:"success",error:"danger",failed:"secondary",done:"info"},e=({row:s})=>t(a,{color:`light-${r[s.task_status]}`,children:s.task_status}),u=s=>t(o,{for:["task_status"],formatterComponent:e,...s});export{u as T};
