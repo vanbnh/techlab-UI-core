f0cb5f7f91481b3320778bde37c5305263b33ea4
vanbnh <bui_ngoc_huy_van@ca-adv.co.jp>
2023-03-09 06:58:55 +0700

    add build prod

--- dist/assets/TaskStatus.033d8426.js
+++
@@ -1,1 +0,0 @@
-import{j as t,au as a}from"./index.4a21e31c.js";import{D as o}from"./react-paginate.9ad0c933.js";const r={success:"success",error:"danger",failed:"secondary",done:"info"},e=({row:s})=>t(a,{color:`light-${r[s.task_status]}`,children:s.task_status}),u=s=>t(o,{for:["task_status"],formatterComponent:e,...s});export{u as T};
