76dec6604b44ddffb70c8483c820a9b13afe5b81
vanbnh <bui_ngoc_huy_van@ca-adv.co.jp>
2023-03-15 08:28:17 +0700

    add build prod

---
+++ dist/assets/TaskStatus.591b31fe.js
@@ -0,0 +1,1 @@
+import{j as t,au as a}from"./index.76fd1bb9.js";import{D as o}from"./react-paginate.45fdd8a1.js";const r={success:"success",error:"danger",failed:"secondary",done:"info"},e=({row:s})=>t(a,{color:`light-${r[s.task_status]}`,children:s.task_status}),u=s=>t(o,{for:["task_status"],formatterComponent:e,...s});export{u as T};
--- dist/assets/TaskStatus.78787b67.js
+++
@@ -1,1 +0,0 @@
-import{j as t,au as a}from"./index.c179c01d.js";import{D as o}from"./react-paginate.ff7a056f.js";const r={success:"success",error:"danger",failed:"secondary",done:"info"},e=({row:s})=>t(a,{color:`light-${r[s.task_status]}`,children:s.task_status}),u=s=>t(o,{for:["task_status"],formatterComponent:e,...s});export{u as T};
